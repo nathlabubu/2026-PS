c817635b725de709b36ce88f7b1bb45689e2bba2
nathlabubu <20251ctb0100061@estudantes.ifpr.edu.br>
2026-09-22 15:11:54 +0000

    BiblioTech: devcontainer Java 21, esqueleto Main.java e .gitignore

---
+++ bibliotech/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Disciplina: 2026-PS
+ * Projeto   : bibliotech
+ * Arquivo   : Main.java
+ * Autor     : seu nome
+ * Descricao : esqueleto do BiblioTech (Aula 36). Ainda nao faz nada:
+ *             so prova que o ambiente compila e roda.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("BiblioTech v0.1 - esqueleto");
+        System.out.println("Casos de uso planejados:");
+        System.out.println("  1. Consultar disponibilidade");
+        System.out.println("  2. Emprestar livro");
+        System.out.println("  3. Devolver livro");
+        System.out.println("  4. Identificar leitor");
+    }
+}
